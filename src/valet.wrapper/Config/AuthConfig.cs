03ebea00e8bbe3e6faee47a493478b8201d47922
João Vidal <dev.joaovidal@gmail.com>
2025-03-12 02:50:31 -0300

    trying to use diferent projects

---
+++ src/valet.wrapper/Config/AuthConfig.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using valet.auth.Core.Interfaces;
+using valet.auth.Service.Hash;
+using valet.auth.Service.Token;
+
+namespace valet.wrapper.Config
+{
+    public static class AuthConfig
+    {
+        public static void AddValetAuthServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var secretKey = configuration.GetValue<string>("Settings:Secret");
+
+            services.AddProviders(secretKey!);
+            services.AddJwt(secretKey!);
+            services.AddAuthSwaggerGen();
+        }
+
+        private static void AddProviders(this IServiceCollection services, string secretKey)
+        {
+            services.AddTransient<IPasswordHasher, PasswordHasher>();
+            services.AddSingleton<ITokenGenerator>(new TokenGenerator(secretKey));
+        }
+
+        private static void AddJwt(this IServiceCollection services, string secretKey)
+        {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey!))
+                };
+            });
+        }
+
+        private static void AddAuthSwaggerGen(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization using Bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+    }
+}
