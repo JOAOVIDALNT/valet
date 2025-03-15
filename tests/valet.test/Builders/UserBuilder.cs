d8984c49d067fa882ed7bcdefb04fa36733e2fa9
João Vidal <dev.joaovidal@gmail.com>
2025-03-14 22:49:16 -0300

    feat: services tests

---
+++ tests/valet.test/Builders/UserBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using valet.lib.Auth.Domain.Entities;
+using valet.lib.Auth.Service.Hash;
+
+namespace valet.test.Builders
+{
+    public static class UserBuilder
+    {
+        public static User Build()
+        {
+            var hasher = new PasswordHasher();
+            return new Faker<User>()
+                .RuleFor(x => x.Id, (x) => x.Random.Guid())
+                .RuleFor(x => x.FirstName, (x) => x.Person.FirstName)
+                .RuleFor(x => x.LastName, (x) => x.Person.LastName)
+                .RuleFor(x => x.Email, (x, y) => x.Internet.Email(y.FirstName + y.LastName))
+                .RuleFor(x => x.Password, (x) => hasher.HashPassword(x.Internet.Password()));
+        }
+    }
+}
---
+++ tests/valet.test/Builders/UserRoleBuilder.cs
@@ -0,0 +1,19 @@
+﻿using valet.lib.Auth.Domain.Entities;
+
+namespace valet.test.Builders
+{
+    public static class UserRoleBuilder
+    {
+        public static UserRole Build(User user, Role role)
+        {
+            return new UserRole
+            {
+                Id = Guid.NewGuid(),
+                User = user,
+                Role = role,
+                UserId = user.Id,
+                RoleId = role.Id
+            };
+        }
+    }
+}
