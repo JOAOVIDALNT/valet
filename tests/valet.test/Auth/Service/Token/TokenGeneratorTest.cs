8cb9099798e61bf551e9204b3099b2e38fe92eef
João Vidal <dev.joaovidal@gmail.com>
2025-04-02 07:27:05 -0300

    feat: add user identifier to payload without encryption

--- tests/valet.test/Auth/Service/Token/TokenGeneratorTest.cs
+++ tests/valet.test/Auth/Service/Token/TokenGeneratorTest.cs
@@ -11,31 +11,18 @@ namespace valet.test.Auth.Service.Token
     public class TokenGeneratorTest
     {
         [Fact]
-        public void Should_Generate_Valid_Token()
-        {
-            var generator = new TokenGenerator("u9yw0akS1fwQh09fA4rpR7ob2t11if41");
-
-            var user = UserBuilder.Build();
-
-            var result = generator.GenerateToken(user);
-
-            Assert.StartsWith("ey", result);
-        }
-
-        [Fact]
         public void Should_Return_Valid_Token()
         {
             var generator = new TokenGenerator("u9yw0akS1fwQh09fA4rpR7ob2t11if41");
 
             var user = UserBuilder.Build();
             var result = generator.GenerateToken(user);
-            //user.FirstName = "xxx";
 
             var handler = new JwtSecurityTokenHandler();
             var jsonToken = handler.ReadToken(result) as JwtSecurityToken;
             var claims = jsonToken?.Claims;
 
-            Assert.Contains(claims, x => x.Value == user.FirstName);
+            Assert.Contains(claims!, x => x.Value == user.Id.ToString());
         }
 
         [Fact]
@@ -45,13 +32,13 @@ namespace valet.test.Auth.Service.Token
 
             var user = UserBuilder.Build();
             var result = generator.GenerateToken(user);
-            user.FirstName = "xxx";
+            user.Id = Guid.NewGuid();
 
             var handler = new JwtSecurityTokenHandler();
             var jsonToken = handler.ReadToken(result) as JwtSecurityToken;
             var claims = jsonToken?.Claims;
 
-            Assert.DoesNotContain(claims, x => x.Value == user.FirstName);
+            Assert.DoesNotContain(claims!, x => x.Value == user.Id.ToString());
         }
     }
 }
