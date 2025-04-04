85e478ff5690a6dd83a6ed8c81ed05b05cc78abd
João Vidal <dev.joaovidal@gmail.com>
2025-04-04 08:10:05 -0300

    feat: setup validate user attribute and filter

---
+++ src/valet.lib/Auth/Service/Token/Middlewares/ValidateUserAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace valet.lib.Auth.Service.Token.Middlewares
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ValidateUserAttribute : TypeFilterAttribute
+    {
+        public ValidateUserAttribute() : base(typeof(ValidateUserFilter)) { }
+
+        public string Roles
+        {
+            get => (string)(Arguments?[0] ?? "");
+            set => Arguments = [value];
+        }
+    }
+}
---
+++ src/valet.lib/Auth/Service/Token/Middlewares/ValidateUserFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using valet.lib.Auth.Domain.Interfaces.Repositories;
+
+namespace valet.lib.Auth.Service.Token.Middlewares
+{
+    public class ValidateUserFilter : IAsyncAuthorizationFilter
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly string _roles;
+        public ValidateUserFilter(IUserRepository userRepository, string roles)
+        {
+            _userRepository = userRepository;
+            _roles = roles;
+        }
+        public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            var token = "";
+
+            throw new NotImplementedException();
+        }
+
+        private static string ExtractToken(AuthorizationFilterContext context)
+        {
+            var header = context.HttpContext.Request.Headers.Authorization.ToString();
+
+            //if (string.IsNullOrWhiteSpace(header))
+            return "";
+                
+        }
+    }
+}
