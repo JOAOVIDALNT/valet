27e9db7754bba137f4fe00b398529a85883e0e71
João Vidal <dev.joaovidal@gmail.com>
2025-07-21 21:38:01 -0300

    feat: base entity properties rebrand in order to be secure

--- src/valet.lib/Core/Domain/Entities/BaseEntity.cs
+++ src/valet.lib/Core/Domain/Entities/BaseEntity.cs
@@ -2,8 +2,13 @@
 {
     public class BaseEntity
     {
-        public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
+        protected void Touch()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
     }
 }
