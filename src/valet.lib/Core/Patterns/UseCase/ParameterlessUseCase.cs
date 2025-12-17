fc4c61935266087133e2384bca09f2d3e5d76b72
João Vidal <dev.joaovidal@gmail.com>
2025-12-16 22:22:39 -0300

    set use cases pattern to accept commands and queries

--- src/valet.lib/Core/Patterns/UseCase/ParameterlessUseCase.cs
+++
@@ -1,6 +0,0 @@
-namespace valet.lib.Core.Patterns.UseCase;
-
-public abstract class ParameterlessUseCase<TResponse>
-{
-    public abstract Task<TResponse?> Execute();
-}
--- src/valet.lib/Core/Patterns/UseCase/UseCase.cs
+++
@@ -1,8 +0,0 @@
-using valet.lib.Core.Domain.Interfaces;
-
-namespace valet.lib.Core.Patterns.UseCase;
-
-public abstract class UseCase<TRequest, TResponse> where TRequest : ISignature
-{
-    public abstract Task<TResponse?> Execute(TRequest signature);
-}
