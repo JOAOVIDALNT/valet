dcf64e2093d0da120c81678e5dfc89c8641846cb
João Vidal <dev.joaovidal@gmail.com>
2026-01-31 01:35:42 -0300

    feat: ensure role exists built in method

--- src/valet.lib/Auth/Domain/Interfaces/Repositories/IRoleRepository.cs
+++ src/valet.lib/Auth/Domain/Interfaces/Repositories/IRoleRepository.cs
@@ -18,5 +18,21 @@ namespace valet.lib.Auth.Domain.Interfaces.Repositories
         /// The task result contains <c>true</c> if a role with the given name exists; otherwise, <c>false</c>.
         /// </returns>
         Task<bool> RoleExistsAsync(string name);
+        /// <summary>
+        /// Retrieves a role with the specified name if it exists;
+        /// otherwise, creates and persists a new role.
+        /// </summary>
+        /// <param name="name">
+        /// The unique name of the role.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> that represents the asynchronous operation.
+        /// The task result contains a <see cref="Role"/> entity tracked by the current context.
+        /// </returns>
+        /// <remarks>
+        /// This method must be used when the returned role will participate
+        /// in write operations. It guarantees that the entity is tracked.
+        /// </remarks>
+        Task<Role> EnsureRoleExistsAsync(string name);
     }
 }
